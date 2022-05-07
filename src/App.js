eb00f7227020bb0f1b5fccf648f9043cfd5ec5ab
Mikail Çelik <mikail.celik@ogr.sakarya.edu.tr>
2022-05-08 00:28:48 +0300

    M-Css dğznlemelri yapıldı Sidebar compenenti ayrıldı

--- src/App.js
+++ src/App.js
@@ -8,10 +8,13 @@ import Sidebar from "./sidebar";
 function App() {
   return (
       <div style={{overflow: "hidden"}}>
+
         <div  id="sidebar">
             <Sidebar/>
         </div>
-        <div style={{marginLeft:40,marginTop:350}}>
+        <div style={{marginLeft:40,marginTop:40}}>
+            <div  style={{height: 400, width: 1200 , margin:"auto",marginBottom:20,backgroundColor:"darkblue"}} >
+            </div>
             <Grid/>
         </div>
       </div>
