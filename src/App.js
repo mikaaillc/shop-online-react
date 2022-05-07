0d1a89e9206f66e341f37c4dd095d2994c205a5d
Mikail Çelik <mikail.celik@ogr.sakarya.edu.tr>
2022-05-07 17:24:11 +0300

    M-Proje kuruldu Grid Side bar eklendi First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { ProSidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
+import 'react-pro-sidebar/dist/css/styles.css';
+import {Grid} from "./grid";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{margin:"left"}}>
+      <ProSidebar  style={{height: 800, width: 200}}>
+
+        <Menu iconShape="square">
+          <MenuItem >Dashboard</MenuItem>
+          <SubMenu title="Components" >
+            <MenuItem>Component 1</MenuItem>
+            <MenuItem>Component 2</MenuItem>
+          </SubMenu>
+
+
+            <SubMenu title="Components" >
+              <MenuItem>Component 1</MenuItem>
+              <SubMenu title="Sub Component 1" >
+
+              </SubMenu>
+            </SubMenu>
+
+        </Menu>
+      </ProSidebar>
+        <div >
+            <Grid/>
+        </div>
+
+
     </div>
   );
 }
---
+++ src/grid.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { render } from 'react-dom';
+import { AgGridReact } from 'ag-grid-react';
+
+
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-balham-dark.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine-dark.css';
+
+
+var axios = require('axios');
+var getresponse;
+var config = {
+    method: 'get',
+    url: 'http://localhost:8080/Product/getAllProduct'
+};
+
+
+axios(config)
+    .then(function (response) {
+        getresponse= response.data;
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
+
+export const Grid = () => {
+
+    const [rowData] = useState(getresponse);
+
+    const [columnDefs] = useState([
+        { field: 'productName' },
+        { field: 'discount' },
+        { field: 'createDate' },
+        { field: 'stock' },
+        { field: 'active' },
+        { field: 'barcode' },
+    ])
+
+    return (
+
+        <div class="ag-theme-balham-dark" style={{height: 400, width: 1000 , margin:"auto"}}  >
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={columnDefs}>
+            </AgGridReact>
+        </div>
+    );
+};
+
+render(<Grid />, document.getElementById('root'));
